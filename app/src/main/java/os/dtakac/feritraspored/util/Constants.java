b440f084b200fe04388434a518ed48b80487fe0c
davidtakac <davidtakac.4@gmail.com>
2020-03-09 13:00:11 +0100

    Bumped target sdk, version code and version name. Small refactors (removed unused logs, strings, spacing etc.)

--- app/src/main/java/os/dtakac/feritraspored/util/Constants.java
+++ app/src/main/java/os/dtakac/feritraspored/util/Constants.java
@@ -1,8 +1,6 @@
 package os.dtakac.feritraspored.util;
 
 public class Constants {
-    public static final String LOG_TAG = "david-log-tag";
-
     public static final String TIMEPICK_LISTENER_KEY = "listener_key";
     public static final String TIMEPICK_TIME_KEY = "initial_time";
     public static final String TIMEPICKER_KEY = "time_picker";
