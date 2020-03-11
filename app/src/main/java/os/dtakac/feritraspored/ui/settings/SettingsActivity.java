d5b3adbbee30125e7388b41ee7e629f30116a082
david.takac <david.takac@factory.hr>
2020-03-11 09:51:35 +0100

    Added toolbar to settings

--- app/src/main/java/os/dtakac/feritraspored/ui/settings/SettingsActivity.java
+++ app/src/main/java/os/dtakac/feritraspored/ui/settings/SettingsActivity.java
@@ -4,33 +4,34 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import os.dtakac.feritraspored.R;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbarSettings)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fl_settings_settingsfragment, new SettingsFragment())
                 .commit();
+        initToolbar();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-            case android.R.id.home: {
-                finish();
-                return true;
-            }
-            default: break;
-        }
-        return super.onOptionsItemSelected(item);
+    private void initToolbar(){
+        toolbar.setNavigationOnClickListener(v -> finish());
     }
 }
