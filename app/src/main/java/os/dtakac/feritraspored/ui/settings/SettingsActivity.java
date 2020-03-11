7f15426b88862dc34a9666cf1aa099b29e3bd5f3
david.takac <david.takac@factory.hr>
2020-03-11 09:45:57 +0100

    Replaced ActionBar with Toolbar, fixed dark theme navbar and status bar colors.

--- app/src/main/java/os/dtakac/feritraspored/ui/settings/SettingsActivity.java
+++ app/src/main/java/os/dtakac/feritraspored/ui/settings/SettingsActivity.java
@@ -16,9 +16,6 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fl_settings_settingsfragment, new SettingsFragment())
