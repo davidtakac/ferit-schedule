6bab48e215f1c4458c47a75105e8f86d7e4333fe
david.takac <david.takac@factory.hr>
2020-03-11 09:13:58 +0100

    Added daynight theme and changed the colors to match dark theme. Light theme WIP

--- app/src/main/java/os/dtakac/feritraspored/util/Constants.java
+++ app/src/main/java/os/dtakac/feritraspored/util/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String TIMEPICK_TIME_KEY = "initial_time";
     public static final String TIMEPICKER_KEY = "time_picker";
     public static final String GROUPSHELP_KEY = "groups_help_dialog";
+    public static final String THEME_KEY = "theme_key";
 }
