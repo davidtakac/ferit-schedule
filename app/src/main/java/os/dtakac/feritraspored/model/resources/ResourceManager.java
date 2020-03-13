b36a42177c8c6164feb0afed38d7e08dc37c57a9
dtakac <pilotrollerball561>
2020-03-13 23:48:09 +0100

    Refactor resource manager wip (presenter done god what was i thinking)

--- app/src/main/java/os/dtakac/feritraspored/model/resources/ResourceManager.java
+++ app/src/main/java/os/dtakac/feritraspored/model/resources/ResourceManager.java
@@ -2,29 +2,9 @@ package os.dtakac.feritraspored.model.resources;
 
 public interface ResourceManager {
 
-    //preference keys
-    String getSkipSaturdayKey();
-    String getSkipDayKey();
-    String getHourKey();
-    String getMinuteKey();
-    String getProgrammeKey();
-    String getYearKey();
-    String getGroupsKey();
-    String getSettingsModifiedKey();
-    String getLoadOnResumeKey();
-    String getPrevDisplayedWeekKey();
-    String getGroupsToggledKey();
-
-    String getScheduleUrl();
-
     //url-ready programme ids
     String getUndergradProgrammeId(int index);
     String getUndergradYearId(int index);
-
-    //user interface strings
-    String getCheckNetworkString();
-    String getCantLoadPageString();
-    String getUnexpectedErrorString();
 
     //file paths
     String getHighlightScriptPath();
