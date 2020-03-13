456a051ec8cf9bc9011d7a0e9f5980feb121a393
dtakac <pilotrollerball561>
2020-03-14 00:11:18 +0100

    Removed unused constants and colors

--- app/src/main/java/os/dtakac/feritraspored/common/util/Constants.java
+++ app/src/main/java/os/dtakac/feritraspored/common/util/Constants.java
@@ -5,8 +5,4 @@ public class Constants {
     public static final String TIMEPICK_TIME_KEY = "initial_time";
     public static final String TIMEPICKER_KEY = "time_picker";
     public static final String GROUPSHELP_KEY = "groups_help_dialog";
-
-    public static final String SYSTEM_THEME = "system";
-    public static final String LIGHT_THEME = "light";
-    public static final String DARK_THEME = "dark";
 }
