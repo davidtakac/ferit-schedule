899bf8d9c56c1f7a80e307accd670600886e586e
davidtakac <davidtakac.4@gmail.com>
2020-11-11 13:36:48 +0100

    refactor SettingsActivity, update gradle, enable view binding

---
+++ app/src/main/java/os/dtakac/feritraspored/settings/activity/SettingsActivity.kt
@@ -0,0 +1,35 @@
+package os.dtakac.feritraspored.settings.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import os.dtakac.feritraspored.R
+import os.dtakac.feritraspored.databinding.ActivitySettingsBinding
+import os.dtakac.feritraspored.settings.fragment.SettingsFragment
+
+class SettingsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        initBinding()
+        initSettingsFragment()
+        initToolbar()
+    }
+
+    private fun initBinding() {
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initSettingsFragment() {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, SettingsFragment())
+                .commit()
+    }
+
+    private fun initToolbar() {
+        binding.toolbarSettings.setNavigationOnClickListener { finish() }
+    }
+}
