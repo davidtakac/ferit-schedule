38c3d8a54756c0b7fe0aaf39dfcc24bd1ca92f6d
davidtakac <davidtakac.4@gmail.com>
2020-11-14 23:16:14 +0100

    add: - week number js - page mod js - todo: webviewclient shenanigans

--- app/src/main/java/os/dtakac/feritraspored/common/scripts/ScriptProvider.kt
+++ app/src/main/java/os/dtakac/feritraspored/common/scripts/ScriptProvider.kt
@@ -3,7 +3,7 @@ package os.dtakac.feritraspored.common.scripts
 interface ScriptProvider {
     fun highlightBlocksFunction(filters: List<String>): String
     fun scrollIntoViewFunction(elementName: String): String
-    fun getWeekNumberFunction(): String
+    fun weekNumberFunction(): String
     fun hideJunkFunction(): String
     fun darkThemeFunction(): String
     fun timeOnBlocksFunction(): String
